538ec6df52f22ffbb58c45e8ccdc1ff83b1fa777
arobbi7 <arobbins@azavea.com>
2017-03-30 10:37:06 -0400

    Labs 1 and 2

--- lab/lab2/js/main.js
+++ lab/lab2/js/main.js
@@ -28,8 +28,8 @@ var Stamen_TonerLite = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_nolab
 // To add visualizations created with the Analysis feature, you will need to export the data to a
 // GeoJSON. From there, you can either import the GeoJSON to Carto or use Leaflet's L.geoJson.
 
-var cartoUserName = 'jfreink';
-var cartoVizId = '1f7c9af2-1088-11e7-9c60-0e05a8b3e3d7';
+var cartoUserName = 'arobbins';
+var cartoVizId = '89b68488-21ef-11e6-b7cf-0ea31932ec1d';
 
 var layerUrl = 'https://'+cartoUserName+'.carto.com/api/v2/viz/'+cartoVizId+'/viz.json';
 
